6d0f526dae31e458f4895a3d8483109156f6c1c9
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-05-27 21:17:09 +0500

    Add scripts for the menu

---
+++ script/main.js
@@ -0,0 +1,26 @@
+//var-s for left pop-up menu
+const leftMenu = document.querySelector('.left-menu');
+const hamburger = document.querySelector('.hamburger');
+
+//open-close menu
+hamburger.addEventListener('click', event => {
+    leftMenu.classList.toggle('openMenu');
+    hamburger.classList.toggle('open');
+});
+document.body.addEventListener('click', event => {
+    if (!event.target.closest('.left-menu')) {
+        leftMenu.classList.remove('openMenu');
+        hamburger.classList.remove('open');
+    }
+});
+
+//dropdowns at the pop-up menu
+leftMenu.addEventListener('click', event => {
+    const target = event.target;
+    const dropdown = target.closest('.dropdown');
+    if (dropdown) {
+        dropdown.classList.toggle('active');
+        leftMenu.classList.add('openMenu');
+        hamburger.classList.add('open');
+    }
+});
